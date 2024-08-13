7853ec36527fae0fc571ea0cf9db2cb90b8dbc07
origami0352 <kyn008116@gmail.com>
2024-08-13 19:57:38 +0900

    리액트 서버 환경변수 지정

--- src/main/frontend/src/setupProxy.js
+++ src/main/frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         createProxyMiddleware({
-            target: 'http://localhost:8080',
+            target: process.env.REACT_APP_SERVER_URL || 'http://localhost:8080',
             changeOrigin: true,
             pathFilter : '/api',
         })
